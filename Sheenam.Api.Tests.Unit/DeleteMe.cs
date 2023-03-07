85835bd5f9d2156fc54d6231a75b13ed163f0e18
nodirShax <burxonovnodirbek8@gmail.com>
2023-03-07 07:12:00 -0800

    INFRA: Unit test project Added

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,16 @@
+﻿//===========================================================
+//CopyRight (c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//===========================================================
+
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+
+        public void SouldbeTrue() => Assert.True(true);
+    }
+}
