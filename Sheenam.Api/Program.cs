68521b09965e5a9df634b6871785a4dca2cf4727
nodirShax <burxonovnodirbek8@gmail.com>
2023-03-07 06:34:53 -0800

    INFRA : Initialize API project

---
+++ Sheenam.Api/Program.cs
@@ -0,0 +1,21 @@
+//===========================================================
+//CopyRight (c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//===========================================================
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Sheenam.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>());
+    }
+}
