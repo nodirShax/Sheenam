e596afaf304e326b6afac5ee7772f847b4edeb8a
nodirShax <burxonovnodirbek8@gmail.com>
2023-03-07 13:09:59 -0800

    INFRA: StorageBroker and Migrate

---
+++ Sheenam.Api/Migrations/20230307210031_InitialMigration.cs
@@ -0,0 +1,42 @@
+﻿//===========================================================
+//CopyRight (c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//===========================================================
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sheenam.Api.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    DateOffBirth = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    Gender = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guests");
+        }
+    }
+}
---
+++ Sheenam.Api/Models/Foundations/Guests/GenderType.cs
@@ -0,0 +1,15 @@
+﻿//===========================================================
+//CopyRight (c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//===========================================================
+
+
+namespace Sheenam.Api.Models.Foundations.Guests
+{
+    public enum GenderType
+    {
+        Male,
+        Female,
+        Other
+    }
+}
